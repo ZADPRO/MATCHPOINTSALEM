4ea6a90ea17053104d2c5b045d35558f49255d81
IONHAX04 <thirukumara04@gmail.com>
2025-03-27 12:22:00 +0530

    home drafted

--- src/components/02-Home/Home.tsx
+++ src/components/02-Home/Home.tsx
@@ -6,8 +6,11 @@ import burger from "../../assets/homePage/burger.png";
 const Home: React.FC = () => {
   return (
     <div>
-      <div className="homePageContainer01 flex lg:flex-row flex-col items-center justify-center h-screen bg-cover bg-center text-white">
-        <div className="text-start lg:pt-0 pt-30 px-6 lg:px-20 flex-1">
+      <div className="homePageContainer01 flex lg:flex-row flex-col items-center justify-center h-screen bg-cover bg-center text-white overflow-auto">
+        <div
+          className="text-start lg:pt-0 pt-30 px-6 lg:px-20 flex-1"
+          data-aos="fade-right"
+        >
           <h1 className="text-4xl lg:text-5xl font-bold mb-4">
             One roof. <br />
             Infinite Happiness. <br />
@@ -18,11 +21,11 @@ const Home: React.FC = () => {
             variety, quality, and innovation in food.
           </p>
         </div>
-        <div className="lg:mt-6 flex-1">
+        <div className="lg:mt-6 flex-1" data-aos="fade-left">
           <img
             src={burger}
             alt="Home Banner"
-            className="w-full max-w-[500px] rounded-lg shadow-lg"
+            className="w-full max-w-[500px] rounded-lg"
           />
         </div>
       </div>
